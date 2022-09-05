9e6311f25e04a288dcb2d1025052c69823412424
Yesid Tombe <y.tombe@uniandes.edu.co>
2022-09-04 23:29:12 -0500

    Cambios en archivo con code duplicado

--- src/pais-restaurante/pais-restaurante.service.ts
+++ src/pais-restaurante/pais-restaurante.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaisEntity } from '../pais/pais.entity';
@@ -133,38 +134,20 @@ export class PaisRestauranteService {
   }
 
   async deleteRestaurantePais(paisId: string, restauranteId: string) {
-    const restaurante: RestauranteEspecializadoEntity =
-      await this.restauranteRepository.findOne({
-        where: { id: restauranteId },
-      });
-    if (!restaurante)
-      throw new BusinessLogicException(
-        'El restaurante con el id indicado no fue encontrado',
-        BusinessError.NOT_FOUND,
-      );
+    const restauranteEsp: RestauranteEspecializadoEntity = await this.restauranteRepository.findOne({where: { id: restauranteId },});
+    if (!restauranteEsp)
+      throw new BusinessLogicException('El restaurante con el id indicado no fue encontrado', BusinessError.NOT_FOUND,);
 
-    const pais: PaisEntity = await this.paisRepository.findOne({
-      where: { id: paisId },
-      relations: ['restaurantesEspecializados'],
-    });
-    if (!pais)
-      throw new BusinessLogicException(
-        'El país con el id indicado no fue encontrado',
-        BusinessError.NOT_FOUND,
-      );
+    const paisEnt: PaisEntity = await this.paisRepository.findOne({where: { id: paisId },relations: ['restaurantesEspecializados'],});
+    if (!paisEnt)
+      throw new BusinessLogicException('El país con el id indicado no fue encontrado', BusinessError.NOT_FOUND,);
 
-    const paisRestaurante: RestauranteEspecializadoEntity =
-      pais.restaurantesEspecializados.find((e) => e.id === restaurante.id);
+    const paisRest: RestauranteEspecializadoEntity = paisEnt.restaurantesEspecializados.find((r) => r.id === restauranteEsp.id);
 
-    if (!paisRestaurante)
-      throw new BusinessLogicException(
-        'El restaurante con el id indicado no está asociado al país',
-        BusinessError.PRECONDITION_FAILED,
-      );
+    if (!paisRest)
+      throw new BusinessLogicException('El restaurante con el id indicado no está asociado al país',BusinessError.PRECONDITION_FAILED,);
 
-    pais.restaurantesEspecializados = pais.restaurantesEspecializados.filter(
-      (e) => e.id !== restauranteId,
-    );
-    await this.paisRepository.save(pais);
+    paisEnt.restaurantesEspecializados = paisEnt.restaurantesEspecializados.filter((r) => r.id !== restauranteId);
+    await this.paisRepository.save(paisEnt);
   }
 }
