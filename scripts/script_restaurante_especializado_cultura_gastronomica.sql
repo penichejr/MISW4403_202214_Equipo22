7fd25fffdb5f17b9a29e103b943eb977c6e32d05
Yesid Tombe <y.tombe@uniandes.edu.co>
2022-09-25 23:58:34 -0500

    Scripts de datos recurso y asociacion

---
+++ scripts/script_restaurante_especializado_cultura_gastronomica.sql
@@ -0,0 +1,3 @@
+INSERT INTO res_esp_ent_cul_gas_cul_gas_ent(
+	"restauranteEspecializadoEntityId", "culturaGastronomicaEntityId")
+	SELECT 'fffdcf47-5fdb-47ce-b3a9-780687a0b391', id FROM cultura_gastronomica_entity
