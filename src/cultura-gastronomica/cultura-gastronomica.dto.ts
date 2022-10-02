9279a21caed37ff38fa38214ddc88d8683562e60
Yesid Tombe <y.tombe@uniandes.edu.co>
2022-10-02 01:12:08 -0500

    GraphQL para asociacion Restaurante-Cultura

--- src/cultura-gastronomica/cultura-gastronomica.dto.ts
+++ src/cultura-gastronomica/cultura-gastronomica.dto.ts
@@ -1,12 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { IsNotEmpty, IsString } from "class-validator";
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
+@InputType()
 export class CulturaGastronomicaDto {
-    
+
+   @Field()
+   @IsString()
+   @IsOptional()
+   readonly id: string;
+   
+   @Field()
    @IsString()
    @IsNotEmpty()
    readonly nombre: string;
 
+   @Field()
    @IsString()
    @IsNotEmpty()
    readonly descripcion: string;
